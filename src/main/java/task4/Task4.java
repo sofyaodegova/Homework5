fa3dcbe98f136335329dc4b0174fbe26ccc6cb64
Sofya Odegova <sophiaodegova@mail.ru>
2022-11-14 20:55:32 +0400

    task 4 is done

---
+++ src/main/java/task4/Task4.java
@@ -0,0 +1,16 @@
+package task4;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Task4 {
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<>();
+        array.add(53);
+        array.add(158);
+        array.add(33);
+        array.add(98);
+        System.out.println("Максимальное значение в массиве: " + Collections.max(array));
+        System.out.println("Минимальное значение в массиве: " + Collections.min(array));
+    }
+}
