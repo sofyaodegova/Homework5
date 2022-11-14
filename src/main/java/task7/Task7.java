4a06e7e3b70424577f553c2594cdbbf16361a957
Sofya Odegova <sophiaodegova@mail.ru>
2022-11-14 21:39:44 +0400

    task 7 is done

---
+++ src/main/java/task7/Task7.java
@@ -0,0 +1,19 @@
+package task7;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Task7 {
+    public static void main(String[] args) {
+        ArrayList<Integer> lottery = new ArrayList<Integer>(100);
+
+        for (int i = 1; i <= 100; i++) {
+            lottery.add(i);
+        }
+
+        Collections.shuffle(lottery);
+        for (int j = 1; j <= 10; j++) {
+            System.out.println(j + " элемент: " + lottery.get(j));
+        }
+    }
+}
