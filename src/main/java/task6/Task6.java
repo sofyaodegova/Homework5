fa58f1148404b541f38f45a49dbad407e7cc4ed2
Sofya Odegova <sophiaodegova@mail.ru>
2022-11-14 21:12:20 +0400

    task 6 is done

---
+++ src/main/java/task6/Task6.java
@@ -0,0 +1,21 @@
+package task6;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Task6 {
+    public static void main(String[] args) {
+        ArrayList<String> solarSystem = new ArrayList<>();
+        solarSystem.add("Mercury");
+        solarSystem.add("Venus");
+        solarSystem.add("Earth");
+        solarSystem.add("Mars");
+        solarSystem.add("Jupiter");
+        solarSystem.add("Saturn");
+        solarSystem.add("Neptune");
+        solarSystem.add("Uranus");
+
+        Collections.swap(solarSystem, 6, 7);
+        System.out.println("Правильный порядок планет Солнечной системы: " + solarSystem);
+    }
+}
