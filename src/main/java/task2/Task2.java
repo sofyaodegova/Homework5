58de82428ba33ab17773a56ecd099ffb872f0521
Sofya Odegova <sophiaodegova@mail.ru>
2022-11-14 20:10:20 +0400

    task 2 is done

---
+++ src/main/java/task2/Task2.java
@@ -0,0 +1,26 @@
+package task2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Task2 {
+    public static void main(String[] args) {
+        ArrayList<String> animals = new ArrayList<>();
+        animals.add("Lion");
+        animals.add("Cat");
+        animals.add("Dog");
+        animals.add("Elephant");
+        animals.add("Giraffe");
+
+        System.out.println("Создан новый массив: " + animals);
+
+        Collections.sort(animals);
+        System.out.println("Массив отсортирован от A до Z: " + animals);
+
+        Collections.reverse(animals);
+        System.out.println("Массив отсортирован от A до Z: " + animals);
+
+        animals.clear();
+        System.out.println("Массив очищен: " + animals);
+    }
+}
